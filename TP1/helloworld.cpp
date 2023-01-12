06c6142aafd4cbe166a0d5cca7d4caf413fac825
Artholgar <arthurpairaud@gmail.com>
2023-01-12 16:54:59 +0100

    Program says Hello + name

--- TP1/helloworld.cpp
+++ TP1/helloworld.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
+#include <string>
 
 int main() 
 {
-    std::cout << "Hello World!" << std::endl;
 
-    auto var = 0;
+    std::cout << "What's your name?" << std::endl;
 
-    std::cin >> var;
+    std::string name;
+
+    std::cin >> name;
+
+    std::cout << "Hello " << name << "!" << std::endl;
 
     return 0;
 }
