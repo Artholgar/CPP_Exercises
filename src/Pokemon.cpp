651f0e844dae9ce3a925a0e55235fd2e7ad614b7
Artholgar <arthurpairaud@gmail.com>
2023-02-07 14:47:53 +0100

    implementing code for test 1

--- src/Pokemon.cpp
+++ src/Pokemon.cpp
@@ -1,1 +1,28 @@
 #include "Pokemon.h"
+
+using namespace std;
+
+int Pokemon::_next_id = 0;
+
+Pokemon::Pokemon(string name) : _id{_next_id}, _name{name} { _next_id++; }
+
+Pokemon::Pokemon(const Pokemon &other) : Pokemon{other._name} {}
+
+Pokemon &Pokemon::operator=(const Pokemon &other)
+{
+    if (this != &other)
+    {
+        _name = other._name;
+    }
+    return *this;
+}
+
+int Pokemon::id() const
+{
+    return _id;
+}
+
+string Pokemon::name() const
+{
+    return _name;
+}
--- src/Pokemon.h
+++ src/Pokemon.h
@@ -4,4 +4,21 @@
 
 // A creature that is cute and can fight other ones.
 class Pokemon
-{};
+{
+public:
+    Pokemon(std::string name);
+
+    Pokemon(const Pokemon &other);
+
+    Pokemon &operator=(const Pokemon &other);
+
+    int id() const;
+
+    std::string name() const;
+
+private:
+    static int _next_id;
+
+    const int _id;
+    std::string _name;
+};
