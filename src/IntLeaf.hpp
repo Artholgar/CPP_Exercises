2235ea3e4d91364692122e88b4d46269a967fbc9
Artholgar <arthurpairaud@gmail.com>
2023-02-21 14:41:43 +0100

    implementing test 1 and 2

--- src/IntLeaf.hpp
+++ src/IntLeaf.hpp
@@ -1,1 +1,22 @@
 #pragma once
+
+#include "Leaf.hpp"
+
+#include <string>
+
+class IntLeaf : public Leaf
+{
+public:
+    IntLeaf(int data)
+        : _data { data }
+    {}
+
+    NodeKind kind() const { return NodeKind::INT; }
+
+    std::string print() const override { return std::to_string(_data); }
+
+    int data() { return _data; }
+
+private:
+    int _data;
+};
--- src/Node.hpp
+++ src/Node.hpp
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "InstanceCounter.hpp"
+
+#include <string>
+
+enum class NodeKind
+{
+    INT
+};
+
+class Node : public InstanceCounter
+{
+public:
+    virtual std::string print() const = 0;
+};
